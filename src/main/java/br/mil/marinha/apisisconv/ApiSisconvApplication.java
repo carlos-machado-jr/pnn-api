58eebb7cfac4073ac97f59860155d6798928c76f
webmaster-pnn <machadopnn@gmail.com>
2021-05-31 11:50:28 -0300

    initial commit

---
+++ src/main/java/br/mil/marinha/apisisconv/ApiSisconvApplication.java
@@ -0,0 +1,13 @@
+package br.mil.marinha.apisisconv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSisconvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSisconvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/mil/marinha/apisisconv/ApiSisconvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mil.marinha.apisisconv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSisconvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
