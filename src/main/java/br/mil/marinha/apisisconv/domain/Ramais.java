56c16943febf70b8be069ddf0ffc3af4658412fe
webmaster-pnn <machadopnn@gmail.com>
2021-05-31 18:40:31 -0300

    criacao do DTO do dominio Ramais

--- src/main/java/br/mil/marinha/apisisconv/domain/Ramais.java
+++ src/main/java/br/mil/marinha/apisisconv/domain/Ramais.java
@@ -1,11 +1,42 @@
 package br.mil.marinha.apisisconv.domain;
 
-public class Ramais {
-	private Long ram_Id;
-	private String ram_Descricao;
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.envers.AuditTable;
+import org.hibernate.envers.Audited;
+
+@Entity
+@Audited
+@AuditTable("ramais_audit")
+public class Ramais implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long ram_Id;
+	
+	private String ram_Descricao;
+	
+	@ManyToOne
+	@JoinColumn(name = "ram_Setor_Id")
 	private Setores setores;
+	
+	@ManyToOne
+	@JoinColumn(name = "ram_Tipo_Id")
 	private TiposRamais tipoRamais;
+	
+	@ManyToOne
+	@JoinColumn(name = "ram_Om_Id")
 	private OrganizacoesMilitares organizacoesMilitares;
 
 	public Ramais() {
---
+++ src/main/java/br/mil/marinha/apisisconv/dto/RamaisDTO.java
@@ -0,0 +1,133 @@
+package br.mil.marinha.apisisconv.dto;
+
+import br.mil.marinha.apisisconv.domain.OrganizacoesMilitares;
+import br.mil.marinha.apisisconv.domain.Setores;
+import br.mil.marinha.apisisconv.domain.TiposRamais;
+
+public class RamaisDTO {
+	
+	private Long id;
+	private String ramal;
+	private String setor;
+	private String tipo;
+	private String organizacaoMilitar;
+	
+	
+	public RamaisDTO(Long id, String ramal, Setores setor, TiposRamais tipo, OrganizacoesMilitares organizacaoMilitar) {
+		super();
+		this.id = id;
+		this.ramal = ramal;
+		this.setor = setor.getSet_Descricao();
+		this.tipo = tipo.getTir_Descricao();
+		this.organizacaoMilitar = organizacaoMilitar.getOm_Descricao();
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getRamal() {
+		return ramal;
+	}
+
+
+	public void setRamal(String ramal) {
+		this.ramal = ramal;
+	}
+
+
+	public String getSetor() {
+		return setor;
+	}
+
+
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+	public String getOrganizacaoMilitar() {
+		return organizacaoMilitar;
+	}
+
+
+	public void setOrganizacaoMilitar(String organizacaoMilitar) {
+		this.organizacaoMilitar = organizacaoMilitar;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((organizacaoMilitar == null) ? 0 : organizacaoMilitar.hashCode());
+		result = prime * result + ((ramal == null) ? 0 : ramal.hashCode());
+		result = prime * result + ((setor == null) ? 0 : setor.hashCode());
+		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RamaisDTO other = (RamaisDTO) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (organizacaoMilitar == null) {
+			if (other.organizacaoMilitar != null)
+				return false;
+		} else if (!organizacaoMilitar.equals(other.organizacaoMilitar))
+			return false;
+		if (ramal == null) {
+			if (other.ramal != null)
+				return false;
+		} else if (!ramal.equals(other.ramal))
+			return false;
+		if (setor == null) {
+			if (other.setor != null)
+				return false;
+		} else if (!setor.equals(other.setor))
+			return false;
+		if (tipo == null) {
+			if (other.tipo != null)
+				return false;
+		} else if (!tipo.equals(other.tipo))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
