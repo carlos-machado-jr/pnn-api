065b10116af74dacc350abc2a0c0dbf2bee47f17
16115597 <16115597@WEBMASTER.pcln2t.ad>
2021-06-01 15:51:19 -0300

    Implementacao da classe de auditoria do Hibernate

--- src/main/java/br/mil/marinha/apisisconv/ApiSisconvApplication.java
+++ src/main/java/br/mil/marinha/apisisconv/ApiSisconvApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiSisconvApplication {
 
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSisconvApplication.class, args);
+		
+	
 	}
 
 }
